60d224ec8ffa3d8e20f77dddb1ca7003f1ce914b
rakesh-modugu <rakeshmodugu94@gmail.com>
2026-05-15 01:03:30 +0530

    fix: increase request payload size limits

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000;
 // Database connection
 await connectDB()
 
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 app.use(cors())
 
 app.get('/', (req, res)=> res.send("Server is live..."))
